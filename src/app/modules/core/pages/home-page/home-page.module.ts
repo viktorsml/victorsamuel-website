e802dcdf166b74cb20b64fd1b93c27427a688a87
Victor Samuel <victorsamuelwd@gmail.com>
2019-07-08 15:23:08 -0500

    Initial home page

--- src/app/modules/core/pages/home-page/home-page.module.ts
+++ src/app/modules/core/pages/home-page/home-page.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { HomePageRoutingModule } from './home-page.routing';
+import { SocialIconsModule } from '../../components/social-icons/social-icons.module';
 
 @NgModule({
   declarations: [HomePageComponent],
   imports: [
     CommonModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    SocialIconsModule
   ]
 })
 export class HomePageModule {}
