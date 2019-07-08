6d8d20a40bb07e9131ba04ee0e1a1ddef91208ad
Victor Samuel <victorsamuelwd@gmail.com>
2019-07-08 13:15:27 -0500

    Improved header

--- src/app/modules/core/components/header/header.component.ts
+++ src/app/modules/core/components/header/header.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuLink {
+  title: string,
+  path: string
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  public menuLinks: Array<MenuLink> = [
+    { title: 'Acerca', path: '/about' },
+    { title: 'Portafolio', path: '/portfolio' },
+    { title: 'Contacto', path: '/contact' },
+  ];
 
   constructor() {}
 
--- src/app/modules/core/core.routing.ts
+++ src/app/modules/core/core.routing.ts
@@ -3,9 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { CoreComponent } from './core.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: '', component: CoreComponent, children: [
-    { path: '', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) },
+    { path: 'about', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) },
     { path: 'portfolio', loadChildren: () => import('./pages/portfolio-page/portfolio-page.module').then(m => m.PortfolioPageModule) },
     { path: 'contact', loadChildren: () => import('./pages/contact-page/contact-page.module').then(m => m.ContactPageModule) },
     { path: '**', loadChildren: () => import('./pages/not-found-page/not-found-page.module').then(m => m.NotFoundPageModule) }
