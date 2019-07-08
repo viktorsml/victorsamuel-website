d6e8a1c5ae25cb5e4f35c1a61a4b59d29d9f410f
Victor Samuel <victorsamuelwd@gmail.com>
2019-07-08 17:22:39 -0500

    Added social icons in header behavior
    Icons will hide if user is in about page

--- src/app/modules/core/components/header/header.component.ts
+++ src/app/modules/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 interface MenuLink {
   title: string,
@@ -12,14 +13,24 @@ interface MenuLink {
 })
 export class HeaderComponent implements OnInit {
 
+  public isSocialBarVisible = false;
   public menuLinks: Array<MenuLink> = [
     { title: 'Acerca', path: '/about' },
     { title: 'Portafolio', path: '/portfolio' },
     { title: 'Contacto', path: '/contact' },
   ];
 
-  constructor() {}
+  constructor(
+    private router: Router
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.isSocialBarVisible = !(this.router.url === '/about');
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isSocialBarVisible = !(event.urlAfterRedirects === '/about');
+      }
+    });
+  }
 
 }
