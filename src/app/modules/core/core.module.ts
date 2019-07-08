d27e62188a2636992d3ab2e8bcb58196c0f901c3
Victor Samuel <victorsamuelwd@gmail.com>
2019-07-08 13:38:50 -0500

    Improved layout

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { CoreComponent } from './core.component';
 import { CoreRoutingModule } from './core.routing';
 import { HeaderComponent } from './components/header/header.component';
+import { SocialIconsComponent } from './components/social-icons/social-icons.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [CoreComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    CoreComponent,
+    HeaderComponent,
+    FooterComponent,
+    SocialIconsComponent
+  ],
   imports: [
     CommonModule,
     CoreRoutingModule
