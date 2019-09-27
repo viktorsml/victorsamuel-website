ea107c0728584cbc748619b04e89d4ea53337802
Victor Samuel <victorsamuelwd@gmail.com>
2019-09-27 12:51:49 -0500

    improves global design

--- src/app/modules/core/pages/home-page/home-page.module.ts
+++ src/app/modules/core/pages/home-page/home-page.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { HomePageRoutingModule } from './home-page.routing';
 import { SocialIconsModule } from '../../components/social-icons/social-icons.module';
+import { PictureModule } from 'src/app/shared/components/picture/picture.module';
 
 @NgModule({
   declarations: [HomePageComponent],
   imports: [
     CommonModule,
     HomePageRoutingModule,
-    SocialIconsModule
+    SocialIconsModule,
+    PictureModule
   ],
   providers: [
     { provide: 'VanillaTilt', useValue: window['VanillaTilt'] }
