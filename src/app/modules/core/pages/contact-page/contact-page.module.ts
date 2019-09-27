690661595e5aa6e391933cabf41b723a2cde29f4
Victor Samuel <victorsamuelwd@gmail.com>
2019-09-27 12:52:08 -0500

    adds contact page

--- src/app/modules/core/pages/contact-page/contact-page.module.ts
+++ src/app/modules/core/pages/contact-page/contact-page.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactPageComponent } from './contact-page.component';
 import { ContactPageRoutingModule } from './contact-page.routing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ContactPageComponent],
   imports: [
     CommonModule,
-    ContactPageRoutingModule
+    ContactPageRoutingModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule 
   ]
 })
 export class ContactPageModule { }
