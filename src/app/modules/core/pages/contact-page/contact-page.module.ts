bb6cee556ef4db694165df1659de8ae48457dd9d
Victor Samuel <victorsamuelwd@gmail.com>
2019-09-28 18:52:13 -0500

    adds page title component

--- src/app/modules/core/pages/contact-page/contact-page.module.ts
+++ src/app/modules/core/pages/contact-page/contact-page.module.ts
@@ -6,16 +6,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { PageTitleModule } from '../../components/page-title/page-title.module';
 
 @NgModule({
   declarations: [ContactPageComponent],
   imports: [
     CommonModule,
     ContactPageRoutingModule,
+    PageTitleModule,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule 
   ]
 })
-export class ContactPageModule { }
+export class ContactPageModule {}
--- src/app/modules/core/pages/portfolio-page/portfolio-page.module.ts
+++ src/app/modules/core/pages/portfolio-page/portfolio-page.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioPageComponent } from './portfolio-page.component';
 import { PortfolioPageRoutingModule } from './portfolio-page.routing';
+import { PageTitleModule } from '../../components/page-title/page-title.module';
 
 @NgModule({
   declarations: [PortfolioPageComponent],
   imports: [
     CommonModule,
-    PortfolioPageRoutingModule
+    PortfolioPageRoutingModule,
+    PageTitleModule
   ]
 })
 export class PortfolioPageModule {}
