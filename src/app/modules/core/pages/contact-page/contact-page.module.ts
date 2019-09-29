457ff851a0aa8ae278646658eababb742301158c
Victor Samuel <victorsamuelwd@gmail.com>
2019-09-29 18:48:19 -0500

    simplifies contact page

--- src/app/modules/core/pages/contact-page/contact-page.module.ts
+++ src/app/modules/core/pages/contact-page/contact-page.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ContactPageComponent } from './contact-page.component';
 import { ContactPageRoutingModule } from './contact-page.routing';
 import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { PageTitleModule } from '../../components/page-title/page-title.module';
 
 @NgModule({
@@ -15,9 +12,6 @@ import { PageTitleModule } from '../../components/page-title/page-title.module';
     ContactPageRoutingModule,
     PageTitleModule,
     MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule 
   ]
 })
 export class ContactPageModule {}
