c1e550db0559bd26a40f0279dc8c8ffc3f42bbf4
Victor Samuel <victorsamuelwd@gmail.com>
2020-01-17 22:05:05 -0600

    Temporary disable proyect page due to being in construction

--- src/app/modules/core/core.routing.ts
+++ src/app/modules/core/core.routing.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/acerca', pathMatch: 'full' },
   { path: '', component: CoreComponent, children: [
     { path: 'acerca', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) },
-    { path: 'proyecto', loadChildren: () => import('./pages/project-page/project-page.module').then(m => m.ProyectPageModule) },
+    // { path: 'proyecto', loadChildren: () => import('./pages/project-page/project-page.module').then(m => m.ProyectPageModule) },
     { path: 'proyectos', loadChildren: () => import('./pages/portfolio-page/portfolio-page.module').then(m => m.PortfolioPageModule) },
     { path: 'contacto', loadChildren: () => import('./pages/contact-page/contact-page.module').then(m => m.ContactPageModule) },
     { path: '**', loadChildren: () => import('./pages/not-found-page/not-found-page.module').then(m => m.NotFoundPageModule) }
