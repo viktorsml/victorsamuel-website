c20b9b805911a1dff49313d772659342ccd49998
Victor Samuel <victorsamuelwd@gmail.com>
2020-03-27 15:52:00 -0600

    Fix tests

--- src/app/modules/core/pages/portfolio-page/portfolio-page.component.spec.ts
+++ src/app/modules/core/pages/portfolio-page/portfolio-page.component.spec.ts
@@ -1,13 +1,22 @@
+import { BehaviorSubject } from 'rxjs';
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { SmartPictureModule } from '../../../../shared/components/smart-picture/smart-picture.module';
+import { SpinnerModule } from '../../../../shared/components/spinner/spinner.module';
 import { PageTitleModule } from '../../components/page-title/page-title.module';
+import { fakeProject } from '../project-page/projects';
 import { PortfolioPageComponent } from './portfolio-page.component';
-import { PortfolioPageRoutingModule } from './portfolio-page.routing';
+
+const FirestoreStub = {
+  collection: (name: string) => ({
+    valueChanges: () => new BehaviorSubject([fakeProject, fakeProject])
+  })
+};
 
 describe('PortfolioPageComponent', () => {
   let component: PortfolioPageComponent;
@@ -16,15 +25,8 @@ describe('PortfolioPageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PortfolioPageComponent],
-      imports: [
-        RouterModule,
-        RouterTestingModule,
-        PortfolioPageRoutingModule,
-        PageTitleModule,
-        SmartPictureModule,
-        MatButtonModule,
-        MatIconModule
-      ]
+      providers: [{ provide: AngularFirestore, useValue: FirestoreStub }],
+      imports: [RouterTestingModule, SpinnerModule, PageTitleModule, SmartPictureModule, MatButtonModule, MatIconModule]
     }).compileComponents();
   }));
 
--- src/app/modules/core/pages/project-page/project-page.component.spec.ts
+++ src/app/modules/core/pages/project-page/project-page.component.spec.ts
@@ -1,10 +1,24 @@
+import { BehaviorSubject, of } from 'rxjs';
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SmartPictureModule } from '../../../../shared/components/smart-picture/smart-picture.module';
+import { SpinnerModule } from '../../../../shared/components/spinner/spinner.module';
 import { PageTitleModule } from '../../components/page-title/page-title.module';
 import { ProjectPageComponent } from './project-page.component';
-import { ProjectPageRoutingModule } from './project-page.routing';
+import { fakeProject } from './projects';
+
+const FirestoreStub = {
+  collection: (name: string) => ({
+    doc: (_id: string) => ({
+      valueChanges: () => new BehaviorSubject(fakeProject)
+    })
+  })
+};
 
 describe('ProjectPageComponent', () => {
   let component: ProjectPageComponent;
@@ -13,7 +27,11 @@ describe('ProjectPageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ProjectPageComponent],
-      imports: [ProjectPageRoutingModule, PageTitleModule, SmartPictureModule, MatButtonModule]
+      providers: [
+        { provide: ActivatedRoute, useValue: { params: of({ projectId: 'test-id' }) } },
+        { provide: AngularFirestore, useValue: FirestoreStub }
+      ],
+      imports: [RouterTestingModule, SpinnerModule, PageTitleModule, SmartPictureModule, MatButtonModule]
     }).compileComponents();
   }));
 
