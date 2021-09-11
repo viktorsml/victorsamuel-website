b4c6419bc40ea81fc0081aabaef18be90ac96923
viktorsml <hola@victorsamuel.com>
2021-09-10 22:08:22 -0500

    🎭 track not found issues in google tag manager

--- apps/website/src/app/layouts/website-layout/pages/not-found-page/not-found-page.component.ts
+++ apps/website/src/app/layouts/website-layout/pages/not-found-page/not-found-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from '@services/analytics';
 import { EnvironmentService } from '@services/environment';
 
 @Component({
@@ -7,17 +8,18 @@ import { EnvironmentService } from '@services/environment';
   styleUrls: ['./not-found-page.component.scss'],
 })
 export class NotFoundPageComponent implements OnInit {
-  constructor(private _environmentService: EnvironmentService) {}
+  constructor(private _environmentService: EnvironmentService, private _analyticsService: AnalyticsService) {}
 
   public async ngOnInit() {
-    await this.beautifyUrlIfUserComesFromARedirectLoop();
+    await this._beautifyUrlIfUserComesFromARedirectLoop();
+    this._trackFailure();
   }
 
   public goBackInNavigationHistory() {
     window.history.back();
   }
 
-  public async beautifyUrlIfUserComesFromARedirectLoop() {
+  private async _beautifyUrlIfUserComesFromARedirectLoop() {
     if (!this._environmentService.isBrowserEnvironment) return;
     const failureUrlCookieName = 'LastKnownUrl';
     const failureUrl = this._environmentService.getCookie(failureUrlCookieName);
@@ -30,6 +32,12 @@ export class NotFoundPageComponent implements OnInit {
       if (error) {
         console.warn('A non critical error happened. You are okay but the URL in you browser may not be looking good.', error);
       }
+    }
+  }
+
+  private _trackFailure() {
+    if (this._environmentService.isBrowserEnvironment) {
+      this._analyticsService.dispatchEvent('Page Not Found', { failureUrl: window.location.href });
     }
   }
 }
