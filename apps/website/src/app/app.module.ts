044ca8135dad53b9ad34b969697e82bda3ac3142
viktorsml <hola@victorsamuel.com>
2021-09-11 15:50:45 -0500

    🦹‍♂️ optimize deployment bundle

--- apps/website/src/app/app.module.ts
+++ apps/website/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { environment } from '@environment';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { WebsiteStateModule } from '@store/website';
 
 import { AppComponent } from './app.component';
@@ -21,12 +21,11 @@ import { AppRoutingModule } from './app.routing';
     WebsiteStateModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({
-      // TODO: Comment this import when ready for production.
-      maxAge: 25, // Retains last 25 states
-      logOnly: environment.production, // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-    }),
+    // StoreDevtoolsModule.instrument({
+    //   maxAge: 25, // Retains last 25 states
+    //   logOnly: environment.production, // Restrict extension to log-only mode
+    //   autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    // }),
     GoogleTagManagerModule.forRoot({
       id: environment.googleTagManagerContainerId,
     }),
