deeb2b7c3ae7ba8746985b020d025676c0bfc0ee
viktorsml <hola@victorsamuel.com>
2021-09-11 00:30:15 -0500

    🍹 fix lighthouse issue

--- apps/website/src/app/components/smart-picture/smart-picture.module.ts
+++ apps/website/src/app/components/smart-picture/smart-picture.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TemplateDirectivesModule } from '@directives/template';
 
 import { SmartPictureComponent } from './smart-picture.component';
 
 @NgModule({
   declarations: [SmartPictureComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TemplateDirectivesModule],
   exports: [SmartPictureComponent],
 })
 export class SmartPictureModule {}
